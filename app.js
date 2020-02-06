bd4c9e707955339bea2f580f7d30b7df3e82934f
Femi Ajayi <femiajayi705@gmail.com>
2020-02-06 02:47:10 -0500

    Added server rules

---
+++ app.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+// Require fs to read from the file system
+const fs = require('fs')
+const express = require('express')
+/**
+* Add https as per the docs
+*/
+const https = require('https')
+const http = require('http')
+const app = express()
+
+// Test Bodyparser
+const bodyParser = require('body-parser')
+// You need to implement cors
+const cors = require('cors')
+/*
+ * Added the following lines to load the builpath
+*/
+const path = require('path')
+
+// Http forcing middleware
+const forceHttps = (req, res, next) => {
+	if(!req.secure){
+		res.redirect(301, 'https://' + req.hostname + req.originalUrl)
+	}
+	next()
+}
+// force https using app.all
+app.all('*', forceHttps)
+// middlewares
+/*
+ * Added to enable buildpath load
+*/
+app.use(express.static(path.join(__dirname, 'build')))
+// middlewares
+/*
+ * Added to enable cors on all requests
+*/
+app.use(cors())
+// Not sure I need this
+app.use(bodyParser.json())
+
+/*
+* Redirect all request to the home page to enable react router
+*/
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+const port = process.env.PORT || 80
+/*
+ * Added to enable https certificate
+*/
+const certificateKeyPath = process.env.CERT_KEY_PATH
+const privateKey  = fs.readFileSync(serverKeyPath, 'utf8')
+const certificate = fs.readFileSync(certificateKeyPath, 'utf8')
+const options = {key: privateKey, cert: certificate}
+/**
+* http and https as per the docs
+*/
+http.createServer(app).listen(port)
+https.createServer(options, app).listen(443)
