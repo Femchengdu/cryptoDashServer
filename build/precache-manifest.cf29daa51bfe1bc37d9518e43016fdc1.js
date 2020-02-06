8869b027d8dfa4d16cdd9f9cade8783a9286d79e
Femi Ajayi <femiajayi705@gmail.com>
2020-02-06 03:14:53 -0500

    Added front end build files

---
+++ build/precache-manifest.cf29daa51bfe1bc37d9518e43016fdc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93f945c7f6bef39d3506b62c20260c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa988070ff94ed6cf9aa",
+    "url": "/static/css/main.11df967e.chunk.css"
+  },
+  {
+    "revision": "f85feaeb17f048dea670",
+    "url": "/static/js/2.a8a1acc1.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/static/js/2.a8a1acc1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa988070ff94ed6cf9aa",
+    "url": "/static/js/main.d93463d5.chunk.js"
+  },
+  {
+    "revision": "c72e9443e2b8db1526ac",
+    "url": "/static/js/runtime-main.bd233369.js"
+  }
+]);
